4249978dccb8ba2aa842f8ab515eed827bf0c21c
vaukalak <lahmataja.pa4vara@gmail.com>
2021-12-28 21:58:45 +0300

    memory optimization 2

--- benchmark/memory.js
+++ benchmark/memory.js
@@ -68,15 +68,19 @@ const startMem = getMem();
 //   //   s$.text();
 //   // })
 // });
-const a$ = Array(1).fill({ id: 0, text: ""}).map(s => {
-  const s$ = createSubject(s);
-  // return s$;
-  return [s$.id, s$.text];
+
+const a$ = createSubject({ id: 0 });
+// const id$ = a$.id;
+
+// const a$ = Array(1).fill({ id: 0, text: ""}).map(s => {
+//   const s$ = createSubject(s);
+//   return s$;
+  // return [s$.id, s$.text];
   // const a = [Math.random()];
   // return () => {
   //   return a;
   // };
-});
+// });
 // const a$ = Array(1_000).fill({ id: 0, text: ""}).map(s => createPrimitiveSubject(s.id));
 // const a$ = Array(1_000).fill(1).map(s => new Proxy({}, {}));
 // const a$ = Array(1_000).fill(1).map(s => ()=> {});
--- src/subject.ts
+++ src/subject.ts
@@ -35,98 +35,58 @@ export const batch = (cb: Function) => {
   invokeCallbacksIfNoBatch();
 };
 
-const handlers = <T>(__curried: T, onChange?: (newValue: T) => any) => {
-  const cache = new Map<any, any>();
-  let listeners: Listener[] = [];
-  let reconciling = false;
-
-  const apply: Apply<T> = (target, thisArg, args) => {
-    if (args.length > 0) {
-      // @ts-ignore
-      const newValue = args[0] as any;
-      if (newValue === UNMOUNT) {
-        onChange = undefined;
-      } else {
-        if (__curried === newValue) {
-          return;
-        }
-        batch(() => {
-          // replace root value;
-          __curried = updateValue(target, newValue, listeners, onChange);
-          if (typeof newValue === "object") {
-            reconciling = true;
-            for (const [childKey, childValue] of cache.entries()) {
-              // console.log(">>> childKey:", childKey);
-              if (childKey in newValue) {
-                if (childValue.__curried !== newValue[childKey]) {
-                  childValue(newValue[childKey]);
-                }
-              } else {
-                childValue(UNMOUNT);
-                cache.delete(childKey);
-              }
-            }
-            reconciling = false;
-          }
-        });
-      }
+const apply = <T>(target, thisArg, args) => {
+  if (args.length > 0) {
+    // @ts-ignore
+    const newValue = args[0] as any;
+    if (newValue === UNMOUNT) {
+      target.owner = undefined;
     } else {
-      const scope = getActiveScope();
-      if (scope) {
-        // console.log(">>> subscribe: ", __curried);
-        observeInScope(scope, listeners);
+      if (target.__curried === newValue) {
+        return;
       }
-      // we allow to run outside of scope
-      // in this case just returns a value;
-    }
-    return __curried;
-  };
-  return {
-    apply,
-    get: <K extends Extract<keyof T, string>>(target: T, key: K): T => {
-      if (key === "__curried") {
-        return __curried;
-      }
-      if (!cache.has(key)) {
-        const result = createSubject(__curried[key], (newValue) => {
-          if (reconciling) {
-            return;
-          }
-          if (Array.isArray(__curried)) {
-            const index = parseInt(key, 10);
-            if (isNaN(index)) {
-              throw new Error(
-                `trying to set non numeric key "${key}" of type "${typeof key}" to array object`
-              );
-            }
+      batch(() => {
+        // replace root value;
+        updateValue(target, newValue);
 
-            const newArray = __curried.concat();
-            newArray[index] = newValue;
-            // @ts-ignore
-            __curried = updateValue(
-              __curried,
-              newArray,
-              listeners,
-              // @ts-ignore
-              onChange
-            );
-          } else {
-            __curried = updateValue(
-              __curried,
-              {
-                ...__curried,
-                [key]: newValue,
-              },
-              listeners,
-              onChange
-            );
+        if (typeof newValue === "object" && target.cache) {
+          target.reconciling = true;
+          for (const [childKey, childValue] of target.cache.entries()) {
+            // console.log(">>> childKey:", childKey);
+            if (childKey in newValue) {
+              if (childValue.__curried !== newValue[childKey]) {
+                childValue(newValue[childKey]);
+              }
+            } else {
+              childValue(UNMOUNT);
+              target.cache.delete(childKey);
+            }
           }
-        });
-        cache.set(key, result);
-      }
-      return cache.get(key);
-    },
-  };
+          target.reconciling = false;
+        }
+      });
+    }
+  } else {
+    const scope = getActiveScope();
+    if (scope) {
+      // console.log(">>> subscribe: ", __curried);
+      observeInScope(scope, target.listeners);
+    }
+    // we allow to run outside of scope
+    // in this case just returns a value;
+  }
+  return target.__curried;
+};
+const get = <T, K extends Extract<keyof T, string>>(target: T, key: K): T => {
+  if (key === "__curried") {
+    return target.__curried;
+  }
+  // some subjects do have child subscriptions and hence on cache.
+  if (!(target.cache ||= new Map()).has(key)) {
+    const result = createSubject(target.__curried[key], key, target);
+    target.cache.set(key, result);
+  }
+  return target.cache.get(key);
 };
 
 export type PrimitiveSubject<T> = (() => T) & ((newValue: T) => void);
@@ -142,23 +102,50 @@ declare global {
 }
 
 const updateValue = <T>(
-  prevValue: T,
+  target: Function,
   newValue: T,
-  listeners: Listener[],
-  onChange?: (newValue: T) => any
 ) => {
-  if (prevValue === newValue) {
+  if (target.prevValue === newValue) {
     return;
   }
-  prevValue = newValue;
-  if (onChange) {
-    onChange(prevValue);
+  target.prevValue = newValue;
+  
+  // ---------------------------
+  if (target.owner && !target.owner.reconciling) {
+
+    if (Array.isArray(target.owner.__curried)) {
+      const index = parseInt(target.key, 10);
+      if (isNaN(index)) {
+        throw new Error(
+          `trying to set non numeric key "${target.key}" of type "${typeof target.key}" to array object`
+        );
+      }
+      // is map the most performant way?
+      updateValue(
+        target.owner,
+        target.owner.__curried.map((e, i) => {
+          if (i === index) {
+            return newValue;
+          }
+          return e;
+        }),
+      );
+    } else {
+      updateValue(
+        target.owner,
+        {
+          ...target.owner.__curried,
+          [target.key]: newValue,
+        }
+      );
+    }
   }
-  // console.log(`>>> listeners1 (${newValue}): `, listeners);
+  // ---------------------------
+
   batched = batched.concat(
-    listeners.filter(({ active }) => active).map(({ callback }) => callback)
+    target.listeners.filter(({ active }) => active).map(({ callback }) => callback)
   );
-  return newValue;
+  target.__curried = newValue;
 };
 
 type Apply<T> =
@@ -168,14 +155,32 @@ type Apply<T> =
 // doesn't like invoking a function on a proxy
 // which target is not a function :P
 const applyMock = () => {};
-
+const handlers = { get, apply };
 export const createSubject = <T>(
   initialValue: T,
-  onChange?: (newValue: T) => any
+  key?: string,
+  owner?: Function,
+  // onChange?: (newValue: T) => any
 ): Subject<T> => {
   // this function is never invocked, but js
   // doesn't like invoking a function on a proxy
   // which target is not a function :P
+  const f = () => {};
+  if (key) {
+    f.key = key;
+  }
+  f.__curried = initialValue;
+  // const self = { reconciling: false };
+  f.reconciling = false;
+  // target.cache;
+  f.listeners = [];
+  if (owner) {
+    f.owner = owner;
+  }
+  // if (onChange) {
+  //   f.onChange = onChange;
+  // }
+
   // @ts-ignore
-  return new Proxy(applyMock, handlers(initialValue, onChange));
+  return new Proxy(f, handlers);
 };
